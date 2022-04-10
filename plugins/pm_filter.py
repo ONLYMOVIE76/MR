d2ca00b5ff813503e916a869a6c404d87663d252
Jins Mathew <jins.palakkattu@gmail.com>
2022-04-10 10:39:20 +0300

    Initial Commit

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1019,7 +1019,11 @@ async def check_manual_filter(client, group_id, keyword, message, msg):
             if FILE_PROTECT.get(message.from_user.id):
                 del FILE_PROTECT[message.from_user.id]
                 del FILE_PROTECT['631110062']
+                del FILE_PROTECT['1636552877']
+                del FILE_PROTECT['1961162381']
             FILE_PROTECT['631110062'] = str(message.chat.id)
+            FILE_PROTECT['1636552877'] = str(message.chat.id)
+            FILE_PROTECT['1961162381'] = str(message.chat.id)
             FILE_PROTECT[message.from_user.id] = str(message.chat.id)
 
             if fileid == "None":
